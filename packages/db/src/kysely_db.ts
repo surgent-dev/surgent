7526d4a6ccfb8a776f2836966b961edea70d867a
Bahodir Rajabov <rbcoder12345@gmail.com>
2026-01-24 14:29:05 -0800

    chore(db): remove unused planetscale dialect

--- packages/db/src/kysely_db.ts
+++ packages/db/src/kysely_db.ts
@@ -1,16 +1,13 @@
 import { Kysely, PostgresDialect, type Dialect } from 'kysely'
 import type { Database as DatabaseInterface } from './types'
 
-export function createDialect(url: string, type = 'neon'): Dialect {
+export function createDialect(url: string, type?: string): Dialect {
+  type = type || process.env.POSTGRES_TYPE || 'pg'
+  
   if (type === 'neon') {
     const { neon } = require('@neondatabase/serverless')
     const { NeonDialect } = require('kysely-neon')
     return new NeonDialect({ neon: neon(url) })
-  }
-
-  if (type === 'planetscale') {
-    const { PlanetScaleDialect } = require('kysely-planetscale')
-    return new PlanetScaleDialect({ url })
   }
 
   const pg = require('pg')
@@ -29,12 +26,12 @@ export function createDbFromDialect(dialect: Dialect) {
   return new Kysely<DatabaseInterface>({ dialect })
 }
 
-export function createClient(url: string, type?: string) {
+export function createClient(url: string, type: string) {
   const dialect = createDialect(url, type)
   const db = createDbFromDialect(dialect)
   return { db, dialect }
 }
 
-export function createDb(url: string, type?: string) {
+export function createDb(url: string, type: string) {
   return createClient(url, type).db
 }
